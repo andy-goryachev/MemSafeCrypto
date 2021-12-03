c5f01cf32978ffc48265bbb4b65a56c6570759cc
andy goryachev <andy@goryachev.com>
2021-12-02 16:13:19 -0800

    test

--- test/goryachev/memsafecrypto/bc/salsa/TestXSalsaTools.java
+++ test/goryachev/memsafecrypto/bc/salsa/TestXSalsaTools.java
@@ -30,16 +30,27 @@ public class TestXSalsaTools
 			CByteArray nonce = TUtils.rndByteArray(XSalsaTools.NONCE_LENGTH_BYTES);
 			CByteArray data = TUtils.rndByteArray(size);
 			
+			// with CByteArray
+			
 			CByteArray os = new CByteArray(size);
 			XSalsaTools.encrypt(key, nonce, data, os, 0, data.length());
-			
 			CByteArray encrypted = os.toReadOnly();
 			
 			CByteArray decrypted = new CByteArray(size); 
-
 			XSalsaTools.decrypt(key, nonce, 0, encrypted.length(), encrypted, decrypted);
 			
 			TF.eq(decrypted.toByteArray(), data.toByteArray());
+			
+			// with byte[]
+			
+			byte[] os2 = new byte[size];
+			XSalsaTools.encrypt(key, nonce.toByteArray(), 0, nonce.length(), data, os2, 0);
+			
+			TF.eq(os2, encrypted.toByteArray());
+			
+			CByteArray decrypted2 = XSalsaTools.decrypt(key, nonce.toByteArray(), 0, nonce.length(), os2, 0, os2.length);
+			
+			TF.eq(decrypted2.toByteArray(), data.toByteArray());
 		}
 	}
 }
